435c2beb08a29c27fe98b0e985592b5a96707721
stathoula <hioteli_st@hotmail.com>
2016-07-30 19:03:59 +0300

    war file changes

--- exercise6/src/main/java/stathoula/Application.java
+++ exercise6/src/main/java/stathoula/Application.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
 }
+
